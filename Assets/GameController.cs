fc689ba8b3df38e1f7068585ee4f76a8185ebc70
zoeschwindt <schwindtzoe380@gmail.com>
2024-10-22 01:07:29 -0300

    Create Menuu

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -10,14 +12,21 @@ public class GameController : MonoBehaviour
 
     private int Kills;
     private int totalEnemies;
+    [SerializeField] private GameObject panelfinjuego;
 
+    
+
+    
     [SerializeField] private TextMeshProUGUI KillsText;
     [SerializeField] private TextMeshProUGUI winText;
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        panelfinjuego.SetActive(false);
         winText.enabled = false;
         totalEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
         UpdateUI();
+        currentTime = startTime;
     }
 
     
@@ -38,6 +47,67 @@ public class GameController : MonoBehaviour
 
     private void Win()
     {
+
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        panelfinjuego.SetActive(true);
+        winText.text = "YOU WIN";
         winText.enabled = true;
+
+
+    }
+
+    // Tiempo inicial en segundos
+    [SerializeField] private float startTime = 60f;
+    private float currentTime; // Tiempo actual del contador
+
+    // Referencia al componente Text de la UI
+    [SerializeField] private TextMeshProUGUI countdownText;
+    
+
+    // Variable para saber si el temporizador ha terminado
+    private bool isTimerFinished = false;
+
+    
+
+    void Update()
+    {
+        if (!isTimerFinished)
+        {
+            // Decrementa el tiempo
+            currentTime -= Time.deltaTime;
+
+            // Si el tiempo llega a cero o menos, ejecuta la acción
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                TimerFinished();
+            }
+
+            
+
+            TimeSpan formatedTime = TimeSpan.FromSeconds(currentTime);
+            countdownText.text = formatedTime.ToString("mm':'ss");
+        }
+    }
+
+    // Método para cuando el temporizador llega a cero
+    void TimerFinished()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        isTimerFinished = true;
+        Debug.Log("¡El tiempo ha terminado!");
+        panelfinjuego.SetActive(true);
+        winText.text = "YOU LOSE";
+        winText.enabled = true;
+
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+        
     }
 }
+
